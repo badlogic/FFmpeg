1b376f777d6e50480dc546840cc7b7608faf7757
badlogic <badlogic@gmail.com>
2022-02-18 21:31:03 +0100

    Windows build, final answer

--- build-windows.sh
+++ build-windows.sh
@@ -15,7 +15,7 @@ rm -rf libogg-1.3.5
 wget http://downloads.xiph.org/releases/ogg/libogg-1.3.5.tar.gz
 tar xzvf libogg-1.3.5.tar.gz
 pushd libogg-1.3.5
-./configure --host=x86_64-w64-mingw32 --prefix=/mingw --disable-shared
+./configure --prefix=/mingw64 --host=x86_64-w64-mingw32 --disable-shared
 make
 make install
 popd
@@ -25,7 +25,7 @@ rm -rf libvorbis-1.3.7
 wget http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.7.tar.gz
 tar xzvf libvorbis-1.3.7.tar.gz
 pushd libvorbis-1.3.7
-./configure --host=x86_64-w64-mingw32 --prefix=/mingw --disable-shared
+./configure --prefix=/mingw64 --host=x86_64-w64-mingw32 --disable-shared
 make -j
 make install
 popd
@@ -35,7 +35,7 @@ rm -rf libvpx
 git clone https://github.com/webmproject/libvpx
 pushd libvpx
 git checkout v1.11.0
-./configure --prefix=/mingw --target=x86_64-win64-gcc --disable-shared
+./configure --prefix=/mingw64 --target=x86_64-win64-gcc --disable-shared
 make -j
 make install
 popd
@@ -45,12 +45,12 @@ rm -rf libwebp
 git clone https://github.com/webmproject/libwebp
 pushd libwebp
 ./autogen.sh
-./configure --prefix=/mingw --host=x86_64-w64-mingw32 --disable-gl --disable-sdl --disable-png --disable-jpeg --disable-tiff --disable-gif --disable-wic --disable-shared
+./configure --prefix=/mingw64 --host=x86_64-w64-mingw32 --disable-gl --disable-sdl --disable-png --disable-jpeg --disable-tiff --disable-gif --disable-wic --disable-shared
 make -j
 make install
 popd
 
-./configure --logfile=configure.log --pkg-config-flags="--static" --prefix=/mingw --extra-cflags="-I/mingw/include" --extra-ldflags="-L/mingw/lib -static" --arch=x86_64 --target-os=mingw64 \
+./configure --logfile=configure.log --pkg-config-flags="--with-path=/usr/local/lib/pkgconfig --static" --arch=x86_64 --target-os=mingw64 \
 			--fatal-warnings --enable-static --disable-shared --disable-ffplay \
 			--disable-doc --disable-htmlpages --disable-manpages --disable-podpages --disable-txtpages \
 			--disable-libxcb --disable-lzma --disable-sdl2 \

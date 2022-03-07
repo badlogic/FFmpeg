5a74b38f705278e30967bb7d77f01f8c1b9bafef
Mario Zechner <badlogicgames@gmail.com>
2022-03-07 15:14:29 +0100

    Strip unnecessary things from build.

--- build-windows.sh
+++ build-windows.sh
@@ -54,6 +54,30 @@ popd
 			--fatal-warnings --enable-static --disable-shared --disable-ffplay \
 			--disable-doc --disable-htmlpages --disable-manpages --disable-podpages --disable-txtpages \
 			--disable-libxcb --disable-lzma --disable-sdl2 \
-			--enable-libwebp --enable-libvpx --enable-libvorbis
+			--disable-avdevice --disable-postproc \
+			--disable-protocols --disable-indevs --disable-outdevs --disable-devices \
+			--disable-encoders --disable-decoders --disable-muxers --disable-demuxers --disable-protocols --disable-filters \
+			--enable-encoder=mjpeg --enable-decoder=mjpeg \
+			--enable-encoder=mpeg4 --enable-decoder=mpeg4 \
+			--enable-encoder=png --enable-decoder=png \
+			--enable-encoder=prores_ks --enable-decoder=prores \
+			--enable-encoder=libwebp_anim \
+			--enable-encoder=libwebp --enable-decoder=webp \
+			--enable-encoder=libvpx_vp9 --enable-decoder=libvpx_vp9 \
+			--enable-encoder=ac3 --enable-decoder=ac3 \
+			--enable-encoder=aac --enable-decoder=aac \
+			--enable-encoder=pcm_s16le --enable-decoder=pcm_s16le \
+			--enable-encoder=libvorbis --enable-decoder=libvorbis \
+			--enable-encoder=rawvideo --enable-decoder=rawvideo \
+			--enable-muxer=avi --enable-demuxer=avi \
+			--enable-muxer=mov --enable-demuxer=mov \
+			--enable-muxer=webp --enable-demuxer=image_webp_pipe \
+			--enable-muxer=webm --enable-demuxer=webm_dash_manifest \
+			--enable-muxer=rawvideo --enable-demuxer=rawvideo \
+			--enable-muxer=pcm_s16le --enable-demuxer=pcm_s16le \
+			--enable-muxer=image2 \
+			--enable-protocol=file --enable-protocol=tcp \
+			--enable-filter=scale --enable-filter=select \
+			--enable-libvpx --enable-libwebp --enable-libvorbis
 make clean
 make -j
